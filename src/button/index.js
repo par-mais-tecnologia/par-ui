793a3d5a7528608e3662fb184910c6639242dbfc
Joao Henrique Cizeski Balestrin <jhbalestrin@gmail.com>
2017-04-06 16:43:49 -0300

    added button, textfield and header components

---
+++ src/button/index.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+
+
+class Button extends Component {
+
+  getButtonStyle() {
+    let defaultStyle = "word-wrap h2 ph3 roboto-regular dif p-f12 justify-around items-center overflow-hidden";
+    let style = "";
+
+    if (this.props.disabled) {
+      style = "button-disabled-background gray-04 cursor-disabled"
+    } else if (this.props.primary) {
+      style = "button-primary-background white b--cottoncandy-02 bb"
+    } else if (this.props.secondary) {
+      style = "button-secondary-background button-secondary-color button-secondary-border"
+    }
+
+    if (this.props.customClassName) {
+      style = style.concat(" ", this.props.customClassName)
+    }
+
+    return style.concat(" ", defaultStyle)
+  }
+
+  render() {
+    let style = this.getButtonStyle();
+    let customStyle = {
+      height: this.props.height,
+      width: this.props.width
+    }
+
+    return (
+      <div style={customStyle} className={style} onClick={this.props.onClick}>
+        <span className="tc">{this.props.label}</span>
+      </div>
+    );
+  }
+}
+
+Button.propTypes = {
+  primary: React.PropTypes.bool,
+  secondary: React.PropTypes.bool,
+  disabled: React.PropTypes.bool,
+  label: React.PropTypes.string,
+  onClick: React.PropTypes.func,
+  height: React.PropTypes.number,
+  width: React.PropTypes.number,
+  customClassName: React.PropTypes.string,
+};
+
+Button.defaultProps = {
+  label: "Primary",
+  onClick: onClick,
+  customStyle:''
+};
+
+function onClick() {
+  console.log("No onClick function defined.");
+}
+
+export default Button;
+
+
--- src/header/index.js
+++ src/header/index.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 
-import '../style/index.css'
+class Header extends Component {
+  render() {
+    return (
+      <div className="h25 flex flex-row justify-between p-shadow-8 bb b--gray-01">
+        <div className="flex flex-row pl3">
+          {this.props.logo && <Link to={this.props.logoPath}>
+            <img className="h25 w4 pointer"
+                 src={this.props.logo}
+                 alt={this.props.logoAlt}
+            />
+          </Link>}
+          {(!this.props.logo) && <div className="h25 w4"/>}
+          <div className="roboto-light gray-01 p-f18 pt3">
+            {this.props.label}
+          </div>
+        </div>
+        {this.props.isLogged &&
+        <div className="flex flex-row pr3">
+          <div className="h25 w25 pa2">
 
-const Header = () =>
-  <div className="mt3"> Header here </div>
+          </div>
+          <div className="p-f14 w4 roboto-regular gray-02 pt3">
+            {this.props.consultantName}
+          </div>
+          <div className="p-link pt3" onClick={this.props.onLogout}>
+            {this.props.exitText}
+          </div>
+        </div>}
+      </div>
+    );
+  }
+}
 
-export default Header
+Header.propTypes = {
+  label: React.PropTypes.string,
+  consultantName: React.PropTypes.string,
+  exitText: React.PropTypes.string,
+  isLogged: React.PropTypes.bool,
+  logoHeight: React.PropTypes.number,
+  logoWidth: React.PropTypes.number,
+  logo: React.PropTypes.string,
+  logoAlt: React.PropTypes.string,
+  logoPath: React.PropTypes.string,
+
+};
+
+Header.defaultProps = {
+  label: "Investimentos",
+  consultantName: "Consultant Name",
+  exitText: "Sair",
+  isLogged: false,
+  logoAlt: "Default img alt",
+  logoPath:'/',
+  onLogout:defaultLogout
+
+
+};
+
+function defaultLogout(){
+  console.log("No logout function defined.");
+}
+export default Header;
+
+
 
