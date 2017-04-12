f4405f6d0a933cb1c6cf7620904041446adebd51
Tiago Pereira <tiago.silva@parmais.com.br>
2017-04-12 09:49:52 -0300

    Fixed header border color

--- src/header/index.js
+++ src/header/index.js
@@ -16,7 +16,7 @@ class Header extends Component {
     } = this.props;
 
     return (
-      <div className="h25 flex flex-row justify-between p-shadow-8 bb b--gray-01 bg-white">
+      <div className="h25 flex flex-row justify-between p-shadow-8 bb b--gray-04 bg-white">
         <div className="flex flex-row pl4">
           {logo && <Link to={logoPath}>
             <img
